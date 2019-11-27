72ebbd2a87ef338d3b5129f27407298ca9c6ae78
Vsevolod <11112365@mail.ru>
2019-11-27 18:56:38 +0300

    Initial Commit

---
+++ Bazis MADI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bazis MADI
+//
+//  Created by Всеволод Андрющенко on 27.11.2019.
+//  Copyright © 2019 Всеволод Андрющенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
